5b6bb978c0e5ac15a629568fbf69497f6e659330
klsc999 <52281410+klsc999@users.noreply.github.com>
2020-02-06 16:42:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,47 +1,63 @@
-// URL API LINE
-$API_URL = 'https://api.line.me/v2/bot/message';
-// ใส่ Channel access token (long-lived)
-$ACCESS_TOKEN = '69RVWDp61sqHRFG/XHTLuQwD/pNkrRDLc3Nu3b9hNxpWBc6tPTt0VMD5bvFOMnP+5cC/jQ1XKZKhMLOFhROQHe7GISbIa6r5LdyS9ERyOOXWrJH78eMElLkWqwqq7NPlUWEyMgsDdnwVjB//aXcQOwdB04t89/1O/w1cDnyilFU=';
-// ใส่ Channel Secret
-$CHANNEL_SECRET = '272e0938e8ce340782b4c5a3ea2b1c24';
+<?php
 
-
-// Set HEADER
+$API_URL = 'https://api.line.me/v2/bot/message/reply';
+$ACCESS_TOKEN = '69RVWDp61sqHRFG/XHTLuQwD/pNkrRDLc3Nu3b9hNxpWBc6tPTt0VMD5bvFOMnP+5cC/jQ1XKZKhMLOFhROQHe7GISbIa6r5LdyS9ERyOOXWrJH78eMElLkWqwqq7NPlUWEyMgsDdnwVjB//aXcQOwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-// Get request content
-$request = file_get_contents('php://input');
-// Decode JSON to Array
-$request_array = json_decode($request, true);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+if ( sizeof($request_array['events']) > 0 )
+{
+
+ foreach ($request_array['events'] as $event)
+ {
+  $reply_message = '';
+  $reply_token = $event['replyToken'];
+
+  if ( $event['type'] == 'message' ) 
+  {
+   if( $event['message']['type'] == 'text' )
+   {
+    $text = $event['message']['text'];
+    $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
+   }
+   else
+    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+  
+  }
+  else
+   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+ 
+  if( strlen($reply_message) > 0 )
+  {
+   //$reply_message = iconv("tis-620","utf-8",$reply_message);
+   $data = [
+    'replyToken' => $reply_token,
+    'messages' => [['type' => 'text', 'text' => $reply_message]]
+   ];
+   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+   echo "Result: ".$send_result."\r\n";
+  }
+ }
+}
+
+echo "OK";
 
 function send_reply_message($url, $post_header, $post_body)
 {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
+ $ch = curl_init($url);
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ $result = curl_exec($ch);
+ curl_close($ch);
 
-    return $result;
+ return $result;
 }
 
-if ( sizeof($request_array['events']) > 0 ) {
-      foreach ($request_array['events'] as $event) {
-      
-      $reply_message = '';
-      $reply_token = $event['replyToken'];
-      $data = [
-         'replyToken' => $reply_token,
-         'messages' => [
-            ['type' => 'text', 
-             'text' => json_encode($request_array)]
-         ]
-      ];
-      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-      $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-      echo "Result: ".$send_result."\r\n";
-   }
-}
-echo "OK";
+?>
